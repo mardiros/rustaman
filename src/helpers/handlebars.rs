0638e21dbcf0f9ad1baca24c5f08f79057221670
Guillaume Gauvrit <guillaume@gauvr.it>
2018-12-08 00:56:50 +0100

    Add a handlebars helper to encode query parameters

--- src/helpers/handlebars.rs
+++ src/helpers/handlebars.rs
@@ -1,5 +1,6 @@
+use url::form_urlencoded;
 use handlebars::{
-    Context, Handlebars, Helper, HelperResult, Output, RenderContext, TemplateRenderError,
+    Context, Handlebars, Helper, HelperResult, Output, RenderContext, TemplateRenderError
 };
 use std::boxed::Box;
 
@@ -19,9 +20,27 @@ fn set_helper(
     Ok(())
 }
 
+// implement via bare function
+fn encode(
+    h: &Helper,
+    _: &Handlebars,
+    _: &Context,
+    rc: &mut RenderContext,
+    out: &mut Output,
+) -> HelperResult {
+
+    let param = h.param(0).and_then(|v| v.value().as_str()).unwrap_or("");
+    let encoded: String = form_urlencoded::Serializer::new(String::new())
+        .append_pair("x", param)
+        .finish();
+    out.write(encoded.get(2..).unwrap_or(""))?;
+    Ok(())
+}
+
 fn get_template_renderer() -> Handlebars {
     let mut hbar = Handlebars::new();
     hbar.register_helper("set", Box::new(set_helper));
+    hbar.register_helper("encode", Box::new(encode));
     hbar
 }
 
